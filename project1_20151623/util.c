931171896eb4450865f39e11b33363c0bc053dee
KOREAN139 <korean139@gmail.com>
2019-03-26 22:47:56 -0700

    <feat> add tokens for lexical analysis C- lang
    remove useless tokens and add essential tokens for lexical analysis

--- project1_20151623/util.c
+++ project1_20151623/util.c
@@ -15,21 +15,28 @@
 void printToken( TokenType token, const char* tokenString )
 { switch (token)
   { case IF:
-    case THEN:
     case ELSE:
-    case END:
-    case REPEAT:
-    case UNTIL:
-    case READ:
-    case WRITE:
+    case INT:
+    case RETURN:
+    case VOID:
+    case WHILE:
       fprintf(listing,
          "reserved word: %s\n",tokenString);
       break;
-    case ASSIGN: fprintf(listing,":=\n"); break;
+    case ASSIGN: fprintf(listing,"=\n"); break;
+    case EQ: fprintf(listing,"==\n"); break;
+    case NEQ: fprintf(listing,"!=\n"); break;
     case LT: fprintf(listing,"<\n"); break;
-    case EQ: fprintf(listing,"=\n"); break;
+    case LTE: fprintf(listing,"<=\n"); break;
+    case GT: fprintf(listing,">\n"); break;
+    case GTE: fprintf(listing,">=\n"); break;
     case LPAREN: fprintf(listing,"(\n"); break;
     case RPAREN: fprintf(listing,")\n"); break;
+    case LBRAC: fprintf(listing,"[\n"); break;
+    case RBRAC: fprintf(listing,"]\n"); break;
+    case LCURLY: fprintf(listing,"{\n"); break;
+    case RCURLY: fprintf(listing,"}\n"); break;
+    case COMMA: fprintf(listing,",\n"); break;
     case SEMI: fprintf(listing,";\n"); break;
     case PLUS: fprintf(listing,"+\n"); break;
     case MINUS: fprintf(listing,"-\n"); break;
@@ -108,7 +115,7 @@ char * copyString(char * s)
 /* Variable indentno is used by printTree to
  * store current number of spaces to indent
  */
-static indentno = 0;
+static int indentno = 0;
 
 /* macros to increase/decrease indentation */
 #define INDENT indentno+=2
