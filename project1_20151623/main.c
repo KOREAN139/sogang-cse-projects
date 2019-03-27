c4cabbed68bccd9a5051d606a4c971fa320d8a05
KOREAN139 <korean139@gmail.com>
2019-03-26 23:34:28 -0700

    <chore> remove unused files & re-write Makefile

--- project1_20151623/main.c
+++ project1_20151623/main.c
@@ -6,29 +6,7 @@
 /****************************************************/
 
 #include "globals.h"
-
-/* set NO_PARSE to TRUE to get a scanner-only compiler */
-#define NO_PARSE FALSE
-/* set NO_ANALYZE to TRUE to get a parser-only compiler */
-#define NO_ANALYZE FALSE
-
-/* set NO_CODE to TRUE to get a compiler that does not
- * generate code
- */
-#define NO_CODE FALSE
-
 #include "util.h"
-#if NO_PARSE
-#include "scan.h"
-#else
-#include "parse.h"
-#if !NO_ANALYZE
-#include "analyze.h"
-#if !NO_CODE
-#include "cgen.h"
-#endif
-#endif
-#endif
 
 /* allocate global variables */
 int lineno = 0;
@@ -44,6 +22,8 @@ int TraceAnalyze = FALSE;
 int TraceCode = FALSE;
 
 int Error = FALSE;
+
+TokenType getToken(void);
 
 int main( int argc, char * argv[] )
 { TreeNode * syntaxTree;
@@ -62,40 +42,7 @@ int main( int argc, char * argv[] )
   }
   listing = stdout; /* send listing to screen */
   fprintf(listing,"\nTINY COMPILATION: %s\n",pgm);
-#if NO_PARSE
   while (getToken()!=ENDFILE);
-#else
-  syntaxTree = parse();
-  if (TraceParse) {
-    fprintf(listing,"\nSyntax tree:\n");
-    printTree(syntaxTree);
-  }
-#if !NO_ANALYZE
-  if (! Error)
-  { if (TraceAnalyze) fprintf(listing,"\nBuilding Symbol Table...\n");
-    buildSymtab(syntaxTree);
-    if (TraceAnalyze) fprintf(listing,"\nChecking Types...\n");
-    typeCheck(syntaxTree);
-    if (TraceAnalyze) fprintf(listing,"\nType Checking Finished\n");
-  }
-#if !NO_CODE
-  if (! Error)
-  { char * codefile;
-    int fnlen = strcspn(pgm,".");
-    codefile = (char *) calloc(fnlen+4, sizeof(char));
-    strncpy(codefile,pgm,fnlen);
-    strcat(codefile,".tm");
-    code = fopen(codefile,"w");
-    if (code == NULL)
-    { printf("Unable to open %s\n",codefile);
-      exit(1);
-    }
-    codeGen(syntaxTree,codefile);
-    fclose(code);
-  }
-#endif
-#endif
-#endif
   fclose(source);
   return 0;
 }
