f053be4fd4a93a473bfbdf4c2f2e59f89690f249
KOREAN139 <korean139@gmail.com>
2019-03-27 04:10:21 -0700

    <chore> prettify code
    I'll follow linux kernel coding style
    
    Ref: https://www.kernel.org/doc/html/v4.10/process/coding-style.html

--- project1_20151623/util.h
+++ project1_20151623/util.h
@@ -1,14 +1,16 @@
 #ifndef _UTIL_H_
 #define _UTIL_H_
 
-/* Procedure printToken prints a token 
+/* 
+ * Procedure printToken prints a token 
  * and its lexeme to the listing file
  */
-void printToken( TokenType, const char* );
+void printToken(TokenType, const char *);
 
-/* Function printTokenTableRow prints row for
+/* 
+ * Function printTokenTableRow prints row for
  * token table
  */
-void printTokenTableRow(TokenType, const char*);
+void printTokenTableRow(TokenType, const char *);
 
 #endif
